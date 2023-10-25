8e94844f911ca15785279791bf4139f9c060e3f3
DangHaDuyLong <longdhdgch211311@fpt.edu.vn>
2023-10-25 13:33:33 +0700

    fix lego

--- routes/detail.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-//const UserModel = require('../models/UserModel');
-var router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('/home');
-})
-
-module.exports = router;
